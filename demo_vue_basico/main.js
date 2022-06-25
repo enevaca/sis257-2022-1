eac646cdeee08bb36cffcb9bc500c37c24fc5969
Noel Vaca <noel.vm.2501@gmail.com>
2022-06-25 11:37:47 -0400

    Finalización demo vue básico introdución

--- demo_vue_basico/main.js
+++ demo_vue_basico/main.js
@@ -3,14 +3,23 @@ const { createApp } = Vue;
 const app = createApp({
   data() {
     return {
+      cart: 0,
       product: "Camiseta",
       image: './assets/images/t-shirt-green-bolivia.jpg',
       inStock: true,
       details: ['50% algodón', '30% lana', '20% poliéster'],
       variants: [
-        { id: 5684, color: 'green' },
-        { id: 5685, color: 'white' }
+        { id: 5684, color: 'green', image: './assets/images/t-shirt-green-bolivia.jpg' },
+        { id: 5685, color: 'white', image: './assets/images/t-shirt-white-bolivia.jpg' }
       ]
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    }
   }
 });
